d95f5248c74d89ca917fc68e070e51834b452eb4
Mike-Testut <michael.testut@gmail.com>
2026-01-14 00:04:32 -0800

    add password length check

---
+++ main.py
@@ -0,0 +1,11 @@
+# check how strong password is (weak, medium, strong) based meeting following criteria:
+# - 8 characters or greater
+# - at least 1 number
+# - mix of upper and lowercase letters
+while True:
+    result = 0
+    password = input("Enter your password: ")
+    # Check for length
+    if len(password) >= 8:
+        result += 1
+
